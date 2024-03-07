a30214acb9bfe866127c81380d4c58f4da722b9b
Panji91 <vksrl1108@naver.com>
2024-03-07 10:55:38 +0900

    CPU 점유율 및 메모리 사용량 정보 관련

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
 from fastapi import FastAPI, Request
-from typing import Union
-from pydantic import BaseModel
 import psutil
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 
-class Item(BaseModel):
-    name : str
-    description : Union[str,None] = None
-    price : float
 
 @app.get("/")
 async def read_root():
     return "This is root path from MyAPI"
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str,  None] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    result = {"item_id": item_id, **item.dict()}
-
-@app.delete("/items/{item_id }")
-def delete_item(item_id: int):
-    return {"deleted": item_id}
 
 @app.get("/info/")
 async def read_info(request:Request) :
